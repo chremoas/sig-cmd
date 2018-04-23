f06d2f0a956a7d436f4a584a8bddd3f4b979906c
Brian Hechinger <wonko@4amlunch.net>
2018-04-23 14:33:17 -0400

    All the role/sig/filter stuff is moved to a separate library.
    This was to de-duplicate the role/sig code as well as to prepare for the future plans of have an API service so this code could be re-used.

--- command/command.go
+++ command/command.go
@@ -1,7 +1,6 @@
 package command
 
 import (
-	"fmt"
 	proto "github.com/chremoas/chremoas/proto"
 	permsrv "github.com/chremoas/perms-srv/proto"
 	rolesrv "github.com/chremoas/role-srv/proto"
@@ -17,20 +16,7 @@ type ClientFactory interface {
 	NewRoleClient() rolesrv.RolesClient
 }
 
-type command struct {
-	funcptr func(ctx context.Context, request *proto.ExecRequest) string
-	help    string
-}
-
 var cmdName = "sig"
-//var commandList = map[string]command{
-//	"list":       {listSigs, "List all SIGs"},
-//	"add":        {addSigs, "Add SIG"},
-//	"remove":     {removeSigs, "Delete SIG"},
-//	"info":       {SigInfo, "Get SIG Info"},
-//	"notDefined": {notDefined, ""},
-//}
-
 var clientFactory ClientFactory
 var role rclient.Roles
 
@@ -52,7 +38,6 @@ func (c *Command) Exec(ctx context.Context, req *proto.ExecRequest, rsp *proto.E
 	cmd.Add("info", &args.Command{SigInfo, "Get SIG info"})
 	cmd.Add("add", &args.Command{addSigs, "Add SIGs"})
 	cmd.Add("remove", &args.Command{removeSigs, "Delete SIGs"})
-	cmd.Add("notDefined", &args.Command{notDefined, ""})
 	err := cmd.Exec(ctx, req, rsp)
 
 	// I don't 100% love this, but it'll do for now. -brian
@@ -87,24 +72,7 @@ func removeSigs(ctx context.Context, req *proto.ExecRequest) string {
 		return common.SendError("Usage: !sig remove <role_name>")
 	}
 
-	canPerform, err := role.Permissions.CanPerform(ctx, req.Sender, []string{"sig_admins"})
-	if err != nil {
-		return common.SendFatal(err.Error())
-	}
-
-	if !canPerform {
-		return common.SendError("User doesn't have permission to this command")
-	}
-
-	roleClient := clientFactory.NewRoleClient()
-
-	// Need to check if it's a sig or not
-	_, err = roleClient.RemoveRole(ctx, &rolesrv.Role{ShortName: req.Args[2]})
-	if err != nil {
-		return common.SendFatal(err.Error())
-	}
-
-	return common.SendSuccess(fmt.Sprintf("Removed: %s\n", req.Args[2]))
+	return role.RemoveRole(ctx, req.Sender, req.Args[2], true)
 }
 
 func SigInfo(ctx context.Context, req *proto.ExecRequest) string {
@@ -112,40 +80,7 @@ func SigInfo(ctx context.Context, req *proto.ExecRequest) string {
 		return common.SendError("Usage: !sig info <role_name>")
 	}
 
-	canPerform, err := role.Permissions.CanPerform(ctx, req.Sender, []string{"sig_admins"})
-	if err != nil {
-		return common.SendFatal(err.Error())
-	}
-
-	if !canPerform {
-		return common.SendError("User doesn't have permission to this command")
-	}
-
-	roleClient := clientFactory.NewRoleClient()
-
-	info, err := roleClient.GetRole(ctx, &rolesrv.Role{ShortName: req.Args[2]})
-	if err != nil {
-		return common.SendFatal(err.Error())
-	}
-
-	return fmt.Sprintf("```ShortName: %s\nType: %s\nFilterA: %s\nFilterB: %s\nName: %s\nColor: %d\nHoist: %t\nPosition: %d\nPermissions: %d\nManaged: %t\nMentionable: %t\nJoinable: %t\n```",
-		info.ShortName,
-		info.Type,
-		info.FilterA,
-		info.FilterB,
-		info.Name,
-		info.Color,
-		info.Hoist,
-		info.Position,
-		info.Permissions,
-		info.Managed,
-		info.Mentionable,
-		info.Joinable,
-	)
-}
-
-func notDefined(ctx context.Context, req *proto.ExecRequest) string {
-	return "This command hasn't been defined yet"
+	return role.RoleInfo(ctx, req.Sender, req.Args[2], true)
 }
 
 func NewCommand(name string, factory ClientFactory) *Command {
@@ -153,8 +88,8 @@ func NewCommand(name string, factory ClientFactory) *Command {
 	role = rclient.Roles{
 		RoleClient:  clientFactory.NewRoleClient(),
 		PermsClient: clientFactory.NewPermsClient(),
-		Permissions: common.Permissions{Client: clientFactory.NewPermsClient()},
+		Permissions: common.Permissions{Client: clientFactory.NewPermsClient(), PermissionsList: []string{"sig_admins"}},
 	}
-	newCommand := Command{name: name, factory: factory}
-	return &newCommand
+
+	return &Command{name: name, factory: factory}
 }
